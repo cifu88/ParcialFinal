26e4ad05cde80c0735c86a4967c143a49f90d785
cifu88 <scifuentes84796@umanizales.edu.co>
2022-05-24 21:24:00 -0500

    Ejercicio herencia

--- Parcial.py
+++ Parcial.py
@@ -1,1 +1,15 @@
+#Ejercicio de herencia:
+
+from datetime import date
+import string
+
+
+class Persona():
+    nombre = string
+    nif = string
+    fechaNac = date
+
+
+class jugador(Persona):
+    numFed : int
 
